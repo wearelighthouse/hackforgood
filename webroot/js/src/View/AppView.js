42a6dec70df5ac5ed7d727e1984b9171c3fbcdbb
cjquinn <christyjquinn@gmail.com>
2016-06-07 11:25:13 +0100

    Some cleanup

--- webroot/js/src/View/AppView.js
+++
@@ -1,9 +0,0 @@
-define(['backbone'], function (Backbone) {
-    var AppView = Backbone.View.extend({
-        initialize: function() {
-            console.log('Hello World');
-        }
-    });
-
-    return AppView;
-});
